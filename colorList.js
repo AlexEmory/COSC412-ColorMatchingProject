7ecba2da7089790586fda2342ccbb7f365369b16
AndrewYoun <43867398+AndrewYoun@users.noreply.github.com>
2018-11-10 16:52:01 -0500

    Add files via upload

---
+++ colorList.js
@@ -0,0 +1,680 @@
+var colorList  = {
+    LightBlue = {
+        name : "LightBlue",
+        L : 51.31,
+        A : -12.79,
+        B : -42
+        },
+        BrightBlue = {
+        name : "BrightBlue",
+        L : 38.5,
+        A : -6.5,
+        B : -39.53
+        },
+        DarkBlue = {
+        name : "DarkBlue",
+        L : 28.7,
+        A : -0.2,
+        B : -39.35
+        },
+        Turquoise = {
+        name : "Turquoise",
+        L : 68.84,
+        A : -36.98,
+        B : -18.41
+        },
+        Gray = {
+        name : "Gray",
+        L : 47.66,
+        A : -3.28,
+        B : -4.81
+        },
+        SemiGlossBLK = {
+        name : "SemiGlossBLK",
+        L : 0,
+        A : 0,
+        B : 0
+        },
+        White = {
+        name : "White",
+        L : 95.13,
+        A : -0.49,
+        B : 6.19
+        },
+        Green = {
+        name : "Green",
+        L : 51.82,
+        A : -47.88,
+        B : 32.96
+        },
+        DarkRed = {
+        name : "DarkRed",
+        L : 37.27,
+        A : 54.14,
+        B : 35.22
+        },
+        Red = {
+        name : "Red",
+        L : 47.96,
+        A : 70.19,
+        B : 45.95
+        },
+        SublimeGreen = {
+        name : "SublimeGreen",
+        L : 72.98,
+        A : -36.06,
+        B : 49.12
+        },
+        Caramel = {
+        name : "Caramel",
+        L : 65.57,
+        A : 24.42,
+        B : 65.13
+        },
+        Yellow = {
+        name : "Yellow",
+        L : 78.48,
+        A : 19.46,
+        B : 78.02
+        },
+        LightYellow = {
+        name : "LightYellow",
+        L : 93.04,
+        A : -7.23,
+        B : 86.58
+        },
+        Sunburst = {
+        name : "Sunburst",
+        L : 50.79,
+        A : 59.62,
+        B : 53.41
+        },
+        Cream = {
+        name : "Cream",
+        L : 81.40,
+        A : 19.51,
+        B : 31.59
+        },
+        LightIvory = {
+        name : "LightIvory",
+        L : 82.74,
+        A : 6.19,
+        B : 26.66
+        },
+        SandBEige = {
+        name : "SandBEige",
+        L : 53.19,
+        A : 9.03,
+        B : 17.69
+        },
+        Honey = {
+        name : "Honey",
+        L : 77.39,
+        A : 19.69,
+        B : 70.57
+        },
+        Tangerine = {
+        name : "Tangerine",
+        L : 68.61,
+        A : 37.43,
+        B : 69.04
+        },
+        Orange = {
+        name : "Orange",
+        L : 58.80,
+        A : 57.09,
+        B : 60.24
+        },
+        LightBrown = {
+        name : "LightBrown",
+        L : 26.93,
+        A : 28.11,
+        B : 38.06
+        },
+        Purple = {
+        name : "Purple",
+        L : 65.18,
+        A : 2.24,
+        B : -22.83
+        },
+        BrightGrape = {
+        name : "BrightGrape",
+        L : 24.10,
+        A : 35.36,
+        B : -43.94
+        },
+        Brown = {
+        name : "Brown",
+        L : 24.05,
+        A : 31.83,
+        B : 35.18
+        },
+        Wood = {
+        name : "Wood",
+        L : 54.41,
+        A : 25.57,
+        B : 30.84
+        },
+        Black = {
+        name : "Black",
+        L : 0.00,
+        A : 0.00,
+        B : 0.00
+        },
+        Maroon = {
+        name : "Maroon",
+        L : 31.11,
+        A : 54.94,
+        B : 8.60
+        },
+        Magenta = {
+        name : "Magenta",
+        L : 55.34,
+        A : 70.21,
+        B : 10.83
+        },
+        HotPink = {
+        name : "HotPink",
+        L : 54.19,
+        A : 68.92,
+        B : -9.58
+        },
+        Lilac = {
+        name : "Lilac",
+        L : 46.23,
+        A : 15.13,
+        B : -29.30
+        },
+        BrightLime = {
+        name : "BrightLime",
+        L : 67.43,
+        A : -32.39,
+        B : 53.95
+        },
+        BrightTeal = {
+        name : "BrightTeal",
+        L : 43.65,
+        A : -28.06,
+        B : -8.92
+        },
+        BrightRed = {
+        name : "BrightRed",
+        L : 53.59,
+        A : 69.34,
+        B : 55.81
+        },
+        Chrome = {
+        name : "Chrome",
+        L : 67.91,
+        A : -2.78,
+        B : 1.29
+        },
+        ComOrange = {
+        name : "ComOrange",
+        L : 57.03,
+        A : 61.73,
+        B : 58.54
+        },
+        BugYellow = {
+        name : "BugYellow",
+        L : 91.52,
+        A : -4.39,
+        B : 85.07
+        },
+        FlatDarkBrown = {
+        name : "FlatDarkBrown",
+        L : 26.16,
+        A : 6.85,
+        B : 7.37
+        },
+        FlatBlack = {
+        name : "FlatBlack",
+        L : 0.00,
+        A : 0.00,
+        B : 0.00
+        },
+        FlatRed = {
+        name : "FlatRed",
+        L : 47.36,
+        A : 67.65,
+        B : 44.96
+        },
+        FlatCobaltBlue = {
+        name : "FlatCobaltBlue",
+        L : 38.42,
+        A : 0.04,
+        B : -44.39
+        },
+        FlatSkyBlue = {
+        name : "FlatSkyBlue",
+        L : 63.80,
+        A : -25.27,
+        B : -33.75
+        },
+        FlatGray = {
+        name : "FlatGray",
+        L : 45.63,
+        A : -1.43,
+        B : -5.44
+        },
+        FlatGreen = {
+        name : "FlatGreen",
+        L : 41.65,
+        A : -17.20,
+        B : 11.80
+        },
+        FlatOliveDrab = {
+        name : "FlatOliveDrab",
+        L : 38.29,
+        A : 3.11,
+        B : 13.12
+        },
+        FlatBrown = {
+        name : "FlatBrown",
+        L : 50.39,
+        A : 28.89,
+        B : 36.27
+        },
+        FlatTan = {
+        name : "FlatTan",
+        L : 54.08,
+        A : 17.03,
+        B : 16.35
+        },
+        FlatWhite = {
+        name : "FlatWhite",
+        L : 94.88,
+        A : -0.01,
+        B : 5.83
+        },
+        FlatYellow = {
+        name : "FlatYellow",
+        L : 77.93,
+        A : 20.24,
+        B : 74.35
+        },
+        FlatLightTan = {
+        name : "FlatLightTan",
+        L : 59.33,
+        A : 35.57,
+        B : 35.14
+        },
+        FlatBeretGreen = {
+        name : "FlatBeretGreen",
+        L : 32.19,
+        A : -32.27,
+        B : 16.99
+        },
+        FlatSeaBlue = {
+        name : "FlatSeaBlue",
+        L : 27.20,
+        A : -4.31,
+        B : -16.27
+        },
+        FlatSteel = {
+        name : "FlatSteel",
+        L : 67.30,
+        A : -0.59,
+        B : 3.78
+        },
+        FlatAluminum = {
+        name : "FlatAluminum",
+        L : 68.41,
+        A : -2.13,
+        B : 0.38
+        },
+        FlatBrass = {
+        name : "FlatBrass",
+        L : 57.97,
+        A : 22.68,
+        B : 44.68
+        },
+        FlatRubber = {
+        name : "FlatRubber",
+        L : 21.29,
+        A : 5.81,
+        B : 3.63
+        },
+        FlatZincChromate = {
+        name : "FlatZincChromate",
+        L : 77.15,
+        A : -9.41,
+        B : 71.43
+        },
+        FlatRust = {
+        name : "FlatRust",
+        L : 26.93,
+        A : 28.11,
+        B : 38.06
+        },
+        FlatViolet = {
+        name : "FlatViolet",
+        L : 44.60,
+        A : 5.82,
+        B : -33.22
+        },
+        FlatSunflower = {
+        name : "FlatSunflower",
+        L : 85.78,
+        A : 6.52,
+        B : 82.11
+        },
+        FlatHunterGrn = {
+        name : "FlatHunterGrn",
+        L : 22.44,
+        A : -21.88,
+        B : 2.62
+        },
+        FlatSplitPea = {
+        name : "FlatSplitPea",
+        L : 50.94,
+        A : -3.11,
+        B : 33.12
+        },
+        FlatGrapefruit = {
+        name : "FlatGrapefruit",
+        L : 44.49,
+        A : 51.04,
+        B : 42.00
+        },
+        FlatCherry = {
+        name : "FlatCherry",
+        L : 39.75,
+        A : 49.64,
+        B : 32.64
+        },
+        FlatGunMetal = {
+        name : "FlatGunMetal",
+        L : 67.91,
+        A : -2.78,
+        B : 1.29
+        },
+        FlatDrkArcrftGry = {
+        name : "FlatDrkArcrftGry",
+        L : 66.11,
+        A : -1.14,
+        B : 7.59
+        },
+        FlatLghtArcrftGry = {
+        name : "FlatLghtArcrftGry",
+        L : 80.69,
+        A : 2.78,
+        B : 13.06
+        },
+        StopLightRed = {
+        name : "StopLightRed",
+        L : 41.68,
+        A : 63.30,
+        B : 45.89
+        },
+        MetallicArcticBlue = {
+        name : "MetallicArcticBlue",
+        L : 29.08,
+        A : -3.24,
+        B : -35.64
+        },
+        TurnSignalAmber = {
+        name : "TurnSignalAmber",
+        L : 79.76,
+        A : 17.37,
+        B : 79.42
+        },
+        MetallicGold = {
+        name : "MetallicGold",
+        L : 61.81,
+        A : 21.24,
+        B : 43.79
+        },
+        MetallicSilver = {
+        name : "MetallicSilver",
+        L : 71.89,
+        A : -1.52,
+        B : 1.15
+        },
+        MetallicCopper = {
+        name : "MetallicCopper",
+        L : 56.73,
+        A : 25.19,
+        B : 39.77
+        },
+        MetallicRed = {
+        name : "MetallicRed",
+        L : 48.05,
+        A : 55.61,
+        B : 18.85
+        },
+        MetallicGraphiteGray = {
+        name : "MetallicGraphiteGray",
+        L : 15.51,
+        A : -0.97,
+        B : 6.15
+        },
+        MetallicBlack = {
+        name : "MetallicBlack",
+        L : 0.00,
+        A : 0.00,
+        B : 0.00
+        },
+        MetalFlakeRed = {
+        name : "MetalFlakeRed",
+        L : 45.52,
+        A : 67.53,
+        B : 44.95
+        },
+        MetalFlakeGreen = {
+        name : "MetalFlakeGreen",
+        L : 49.04,
+        A : -41.98,
+        B : 17.85
+        },
+        MetalFlakePurple = {
+        name : "MetalFlakePurple",
+        L : 39.52,
+        A : 37.98,
+        B : -7.06
+        },
+        MetalFlakeBlue = {
+        name : "MetalFlakeBlue",
+        L : 45.95,
+        A : -7.12,
+        B : -45.31
+        },
+        MetalFlakeGold = {
+        name : "MetalFlakeGold",
+        L : 77.15,
+        A : -9.41,
+        B : 71.43
+        },
+        MetallicBlackPearl = {
+        name : "MetallicBlackPearl",
+        L : 10.69,
+        A : -0.30,
+        B : -0.86
+        },
+        FluorescentOrange = {
+        name : "FluorescentOrange",
+        L : 66.98,
+        A : 41.56,
+        B : 56.15
+        },
+        FluorescentGreen = {
+        name : "FluorescentGreen",
+        L : 72.45,
+        A : -36.70,
+        B : 39.05
+        },
+        FluorescentRed = {
+        name : "FluorescentRed",
+        L : 60.81,
+        A : 52.92,
+        B : 59.48
+        },
+        FluorescentBlue = {
+        name : "FluorescentBlue",
+        L : 53.90,
+        A : -13.29,
+        B : -43.59
+        },
+        FluorescentYellow = {
+        name : "FluorescentYellow",
+        L : 84.98,
+        A : -18.96,
+        B : 72.22
+        },
+        FluorescentPink = {
+        name : "FluorescentPink",
+        L : 59.91,
+        A : 60.91,
+        B : 6.65
+        },
+        TransparentBlue = {
+        name : "TransparentBlue",
+        L : 48.35,
+        A : -9.30,
+        B : -44.39
+        },
+        CustomGreen = {
+        name : "CustomGreen",
+        L : 61.86,
+        A : -45.49,
+        B : 8.20
+        },
+        CustomRed = {
+        name : "CustomRed",
+        L : 45.52,
+        A : 67.53,
+        B : 44.95
+        },
+        CustomHotRodRed = {
+        name : "CustomHotRodRed",
+        L : 48.37,
+        A : 71.24,
+        B : 39.71
+        },
+        CustomGrape = {
+        name : "CustomGrape",
+        L : 45.10,
+        A : 70.30,
+        B : 14.65
+        },
+        CustomBlack = {
+        name : "CustomBlack",
+        L : 44.22,
+        A : -4.92,
+        B : 2.00
+        },
+        DiamondDust = {
+        name : "DiamondDust",
+        L : 77.46,
+        A : -1.27,
+        B : 1.76
+        },
+        FieryOrange = {
+        name : "FieryOrange",
+        L : 53.33,
+        A : 42.60,
+        B : 55.67
+        },
+        BlazingBlack = {
+        name : "BlazingBlack",
+        L : 11.47,
+        A : -1.25,
+        B : -1.19
+        },
+        WhiteLightning = {
+        name : "WhiteLightning",
+        L : 95.21,
+        A : 0.01,
+        B : 4.25
+        },
+        LimeIce = {
+        name : "LimeIce",
+        L : 67.56,
+        A : -33.81,
+        B : 53.24
+        },
+        DeJaBlue = {
+        name : "DeJaBlue",
+        L : 31.19,
+        A : 2.12,
+        B : -44.56
+        },
+        RevvingRed = {
+        name : "RevvingRed",
+        L : 40.37,
+        A : 61.90,
+        B : 42.09
+        },
+        MythicalMaroon = {
+        name : "MythicalMaroon",
+        L : 34.88,
+        A : 56.91,
+        B : 36.71
+        },
+        IncaGold = {
+        name : "IncaGold",
+        L : 73.23,
+        A : 17.96,
+        B : 73.38
+        },
+        FlamingOrange = {
+        name : "FlamingOrange",
+        L : 42.02,
+        A : 56.30,
+        B : 46.36
+        },
+        ElectricPink = {
+        name : "ElectricPink",
+        L : 52.39,
+        A : 73.37,
+        B : -3.82
+        },
+        PurpleLicious = {
+        name : "PurpleLicious",
+        L : 15.99,
+        A : 29.67,
+        B : -43.84
+        },
+        StarSpangledBlue = {
+        name : "StarSpangledBlue",
+        L : 37.17,
+        A : -4.55,
+        B : -41.05
+        },
+        IcyBlue = {
+        name : "IcyBlue",
+        L : 58.81,
+        A : -18.4,
+        B : -40.38
+        },
+        MysticEmerald = {
+        name : "MysticEmerald",
+        L : 57.10,
+        A : -49.96,
+        B : 29.27
+        },
+        PureGold = {
+        name : "PureGold",
+        L : 68.06,
+        A : 19.87,
+        B : 64.78
+        },
+        Bronze = {
+        name : "Bronze",
+        L : 52.18,
+        A : 11.70,
+        B : 51.24
+        },
+        RootBeer = {
+        name : "RootBeer",
+        L : 13.15,
+        A : 32.99,
+        B : 20.77
+        },
+        GraphiteDust = {
+        name : "GraphiteDust",
+        L : 43.82,
+        A : -1.01,
+        B : 2.12
+        }
+};
